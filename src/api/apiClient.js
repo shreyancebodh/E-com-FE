b6928d9df5d65ecc35536ef299ec0876f577d13b
Shreyance <shreyancebodh2013@gmail.com>
2025-01-21 14:53:01 +0530

    abckend api update

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000', // Your backend URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
